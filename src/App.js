3cb3375790ef97ac6ac184341ee677dce244d804
Shubham Singh <mynameshubham@gmail.com>
2019-02-28 16:06:00 +0400

    implementing search box

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { InfoWindow, Marker } from 'google-maps-react';
 
 import CurrentLocation from './Map';
+// import SearchBox from "./SearchBox";
 
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
+const btn =  {
+  borderRadius: '0px'
+}
+
+
 export class MapContainer extends Component {
   state = {
+    input: '',
     showingInfoWindow: false,  //Hides or the shows the infoWindow
     activeMarker: {},          //Shows the active marker upon click
     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
@@ -30,12 +37,15 @@ export class MapContainer extends Component {
       });
     }
   };
+  onBtnClick = () => {
+    this.setState({ input: document.getElementById('searchBox').value });
+  }
   render() {
     return (
         <div className="container-fluid">
-          <h1 className="mt-5">React Uber App</h1>
-          <div className="row mt-5">
-            <div className="col-6" style={{height: '600px'}}>
+          <h1 className="mt-3">React Uber App</h1>
+          <div className="row mt-3">
+            <div className="col-8" style={{height: '500px'}}>
               <CurrentLocation
                   centerAroundCurrentLocation
                   google={this.props.google}
@@ -51,8 +61,15 @@ export class MapContainer extends Component {
                   </div>
                 </InfoWindow>
               </CurrentLocation>
+              <div className="mt-3">
+                {/*<SearchBox input={this.state.input}/>*/}
+              </div>
             </div>
-            <div className="col-6">
+            <div className="col-4">
+              <div className="m-2">
+                <input id="searchBox" className="input p-2 w-100" placeholder="search location..." />
+                <button className="button btn btn-info mt-1" style={btn}>search</button>
+              </div>
             </div>
           </div>
         </div>
